ec78c6730b46eca42ebba671cc33b957aaadb5fc
mumu <932852117@qq.com>
2019-05-25 00:43:13 +0800

    重构

--- src/main/java/com/xrlj/utils/authenticate/JwtUtils.java
+++ src/main/java/com/xrlj/utils/authenticate/JwtUtils.java
@@ -3,16 +3,14 @@ package com.xrlj.utils.authenticate;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.xrlj.utils.PrintUtil;
 import com.xrlj.utils.security.Base64Utils;
 
-import java.io.UnsupportedEncodingException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
+import java.time.Instant;
 import java.util.Date;
 
 /**
@@ -20,28 +18,74 @@ import java.util.Date;
  */
 public final class JwtUtils {
 
-    private JwtUtils(){}
+    //token过期时间，默认5分钟
+    private static final long EXPIRE_TIME_DEFAULT = 5 * 60 * 1000; //毫秒
 
-    public static String genAnSignToken(long userId,String username,String userType, String secret, long time) throws Exception {
-        long expTime = System.currentTimeMillis() + time; //到期时间，当前时间延后time时长
-        Algorithm algorithm = Algorithm.HMAC256("secret");
-        String token = JWT.create()
-                .withIssuer("xrlj")
-                .withExpiresAt(new Date(expTime))
-                .withClaim("userId", userId)
-                .withClaim("username",username)
-                .withClaim("userType", userType)
-                .sign(algorithm);
-        return token;
+    private JwtUtils() {
     }
 
-    public static boolean verifyToken() {
+    /**
+     * 生成签名的token。
+     *
+     * @param userId     用户id
+     * @param username   用户名
+     * @param userType   用户类型
+     * @param secret     签名秘钥，保存在服务端
+     * @param millisTime 过期时间，毫秒
+     * @return 返回签名的jwt，未加密。否则返回空字符串。
+     * @throws Exception
+     */
+    public static String genAnSignToken(long userId, String username, String userType, String secret, long millisTime) {
+        try {
+            if (millisTime <= 0) {
+                millisTime = EXPIRE_TIME_DEFAULT;
+            }
+            //到期时间，当前时间延后time时长,单位毫秒
+            Instant expTime = Instant.now().plusMillis(millisTime);
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("xrlj")
+                    .withExpiresAt(new Date(expTime.toEpochMilli()))
+                    .withClaim("userId", userId)
+                    .withClaim("username", username)
+                    .withClaim("userType", userType)
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception) {
+            exception.printStackTrace();
+        }
+        return "";
+    }
 
+    public static boolean verifyToken(long userId, String username, String userType, String secret,String token) throws Exception {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("xrlj")
+                    .withClaim("userId", userId)
+                    .withClaim("username", username)
+                    .withClaim("userType", userType)
+                    .build();
+            DecodedJWT jwt = verifier.verify(token);
+            return true;
+        } catch (JWTVerificationException e) {
+            throw e;
+        }
     }
 
     public static void main(String[] args) throws Exception {
-//        String token = createAnSignToken();
-//        System.out.println(token);
-        verifyToken();
+//        String token = genAnSignToken(111, "zmt","a","abc",3 *60 * 1000);
+//        PrintUtil.println(token);
+
+        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ4cmxqIiwidXNlclR5cGUiOiJhIiwiZXhwIjoxNTU4NzE1NDE3LCJ1c2VySWQiOjExMSwidXNlcm5hbWUiOiJ6bXQifQ.Twwsp0OAnwL-tHv_wSkVryEVQwU2-uTGfqT97S9HCvs";
+//        boolean a = verifyToken(111,"zmt","a","abc",token);
+//        PrintUtil.println(a);
+
+        try {
+            DecodedJWT jwt = JWT.decode(token);
+            PrintUtil.println(Base64Utils.base64Decode(jwt.getPayload()));
+        } catch (JWTDecodeException exception){
+            //Invalid token
+        }
     }
 }
